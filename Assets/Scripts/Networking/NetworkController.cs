0a94e5ad20c4ed598b2f29feb68a94fa22dbd162
jonathanindetzki <h.indetzki@gmx.de>
2020-03-26 16:36:42 +0100

    Added Events to NetworkController

--- Assets/Scripts/Networking/NetworkController.cs
+++ Assets/Scripts/Networking/NetworkController.cs
@@ -6,6 +6,27 @@ namespace DigitaleSpielekiste
 {
     public static class NetworkController
     {
+        #region GlobalMethods
+
+        private static RoomController room;
+
+        /// <summary>
+        /// Checks if the Device is connected to a Room.
+        /// </summary>
+        public static bool HasRoom() {
+            return room != null;
+        }
+
+        /// <summary>
+        /// Returns the Room
+        /// </summary>
+        /// <remarks>
+        /// Device has to be connected to a room or else this will return <c>null</c>
+        /// </remarks>
+        public static RoomController GetRoom() {
+            return room;
+        }
+
         /// <summary>
         /// Creates a new Room
         /// </summary>
@@ -13,14 +34,13 @@ namespace DigitaleSpielekiste
         /// Tries to create a new room with a random ID.
         /// </remarks>
         /// <returns>
-        /// Callback contains the created Room, which is an Instance of the RoomController.
+        /// If successful it will call the <c>onCreatedRoom</c> and then the <c>onJoinedRoom</c> event.
         /// </returns>
         /// <param name="playerSettings">An Instance of <c>PlayerSettings</c></param>
         /// <param name="roomSettings">An Instance of <c>RoomSettings</c> without the ID specified.</param>
         public static void CreateRoom(
             PlayerSettings playerSettings,
-            RoomSettings roomSettings,
-            Event<RoomController> callback
+            RoomSettings roomSettings
         ) {
 
         }
@@ -32,16 +52,58 @@ namespace DigitaleSpielekiste
         /// Tries to join a room with the given ID.
         /// </remarks>
         /// <returns>
-        /// Callback contains the Room settings of the joined Room.
+        /// Callback contains an Instance of the <c>RoomController</c> for the joined Room.
         /// </returns>
         public static void JoinRoom(
             PlayerSettings playerSettings,
-            string roomId,
-            Event<RoomController> callback
+            string roomId
         ) {
 
         }
 
-        public delegate void Event<T>(T args);
+        #endregion
+
+        #region Events
+
+        public static NetworkEvent<RoomController> onJoinedRoom = delegate {};
+
+        public static NetworkEvent<RoomController> onCreatedRoom = delegate {};
+
+        public static NetworkEvent<string> onLeftRoom = delegate {};
+
+        public static NetworkEvent<string> onLostConnection = delegate {};
+
+
+        /// Add Event Listeners of return-type <c>RoomController</c>
+        public static bool addEventListener(string name, NetworkEvent<RoomController> callback){
+            switch(name) {
+                case "joined-room":
+                    onJoinedRoom += callback;
+                    return true;
+                case "created-room":
+                    onCreatedRoom += callback;
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// Add Event Listeners with return-type <c>String</c>
+        public static bool addEventListener(string name, NetworkEvent<string> callback) {
+            switch(name) {
+                case "left-room":
+                    onLeftRoom += callback;
+                    return true;
+                case "lost-connection":
+                    onLostConnection += callback;
+                    return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        public delegate void NetworkEvent<T>(T args);
     }
 }
